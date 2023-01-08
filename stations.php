f0dfb26418ac52bd307d4b3b08ad05486ae0342f
Abdu114 <abdullahishino123@gmail.com>
2023-01-08 03:33:13 +0200

    I don't remember what I changed XD

--- stations.php
+++ stations.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--font-->
+		<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
     <title>City Bike || Stations List VIew</title>
     <style>
       html {
