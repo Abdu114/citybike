b8fcdae9aa65d552920162f2c5908cc872ba87ec
Abdu114 <abdullahishino123@gmail.com>
2023-01-08 04:00:05 +0200

    modified the sidenav code

--- journey.js
+++ journey.js
@@ -1,4 +1,5 @@
 const searchBox = document.getElementById('icon_prefix').addEventListener('keyup', search);
+const searchBoxSmall = document.getElementById('icon_prefix_small').addEventListener('keyup', search_small);
       
 function search(e) {
   //get card
@@ -29,3 +30,38 @@ function search(e) {
     card.style.display = 'none';
   }
 }
+// for small screens search box      
+function search_small(e) {
+  //get card
+  const card = document.getElementById('card_small');
+  const cardValue = e.target.value;
+  // input is not empty
+  if(e.target.value !== ''){
+    card.style.display = 'block';
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'journey_ajax.php?j=' + cardValue, true);
+
+    xhr.onprogress = function(){
+    }
+    xhr.onload = function(){
+      if (xhr.readyState == 4 && xhr.status == 200) { 
+        // Put the result in DOM
+        card.innerHTML = xhr.responseText;
+      }
+      else{
+        console.log('error');
+      }
+    }
+    xhr.send();
+  }
+  else{
+    card.style.display = 'none';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
