909255cbba13ff86dc7e65551fa1e1e34f00655c
Abdu114 <abdullahishino123@gmail.com>
2023-01-09 03:27:51 +0200

    added google map, and total journeys calculations

--- stations-single.php
+++ stations-single.php
@@ -1,3 +1,40 @@
+<?php 
+  include('db.php');
+
+  // GEt the first station name to use in the link if there is no station name in the link
+  $first_station_sql = "SELECT nimi FROM stations ORDER BY nimi ASC LIMIT 1";
+  $first_station_query = mysqli_query($db, $first_station_sql);
+  $first_station_fetch = mysqli_fetch_assoc($first_station_query);
+  $first_station_res_name = $first_station_fetch['nimi'];
+  // if the link is empty get the first station name in DB
+  $station_name = isset($_GET['station']) ? $_GET['station'] : $first_station_res_name;
+
+  // station in the link
+  $station_link_sql = "SELECT nimi, namn, x, y, osoite FROM stations WHERE nimi = '$station_name'";
+  $station_link_query = mysqli_query($db, $station_link_sql);
+
+  while($station_link_fetch = mysqli_fetch_assoc($station_link_query)){
+    $station_link_res_name = $station_link_fetch['nimi'];
+    $station_link_res_namn = $station_link_fetch['namn'];
+    $station_link_res_osoite = $station_link_fetch['osoite'];
+    $station_link_res_x = $station_link_fetch['x'];
+    $station_link_res_y = $station_link_fetch['y'];
+  }
+
+  // Total journeys started
+  $total_journeys_started_sql = "SELECT count(journeys.departure_station_id) as startedjourneys FROM journeys INNER JOIN stations ON journeys.departure_station_id = stations.id WHERE stations.nimi = '$station_link_res_name'";
+  $total_journeys_started_query = mysqli_query($db, $total_journeys_started_sql);
+  $total_journeys_started_fetch = mysqli_fetch_assoc($total_journeys_started_query);
+  $total_journeys_started_res = $total_journeys_started_fetch['startedjourneys'];
+  
+  // Total journeys ended
+  $total_journeys_ended_sql = "SELECT count(journeys.return_station_id) as endedjourneys FROM journeys INNER JOIN stations ON journeys.return_station_id = stations.id WHERE stations.nimi = '$station_link_res_name'";
+  //echo $total_journeys_ended_sql;
+  $total_journeys_ended_query = mysqli_query($db, $total_journeys_ended_sql);
+  $total_journeys_ended_fetch = mysqli_fetch_assoc($total_journeys_ended_query);
+  $total_journeys_ended_res = $total_journeys_ended_fetch['endedjourneys'];
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -65,26 +102,29 @@
         border-bottom: 1px solid #000;
         padding: 10px;
         font-size: 1.2rem;
-        /*margin-bottom: 3px;*/
         cursor: pointer;
       }
       .collection-items.active{
         background-color: #000;
         color: #fff;
         transform: scale(1.03);
-        transition: 0.5s;
+        transition: 0.3s;
         -ms-transform: scale(1.03);
         -webkit-transform: scale(1.03);
       }
       .collection-items:hover{
         transform: scale(1.03);
-        transition: 0.5s;
+        transition: 0.3s;
         -ms-transform: scale(1.03);
         -webkit-transform: scale(1.03);
-
+      }
+      a{
+        color: #000;
       }
       ::-webkit-scrollbar {
-        width: 10px;
+        width: 7px;
+        height: 7px;
+
       }
       
       ::-webkit-scrollbar-track {
@@ -93,18 +133,37 @@
       }
       
       ::-webkit-scrollbar-thumb {
-        background: 	hsl(0, 0%, 1%);
+        background: hsl(0, 0%, 1%);
         height: 150px;
-
       }
       
       ::-webkit-scrollbar-thumb:hover {
         background: 	hsl(0, 0%, 25%);
       }
+      #map {
+        height: 400px; /* The height is 400 pixels */
+        width: 100%; /* The width is the width of the web page */
+      }
+      #right-align{
+        float: right;
+      }
+      #left-col{
+        padding-left: 90px;
+      }
+      .label{
+        margin-bottom: -12px;
+      }
+      .bold-text{
+        font-weight: 600;
+      }
 			@media only screen and (max-width: 992px) {
 				nav{
 					width: 100%;
 			  }
+        .collections{
+        max-height: 300px;
+        overflow: overlay;
+      }
 			}
     </style>
   </head>
@@ -132,7 +191,7 @@
       <!-- List box -->
       <div class="col l3 s12 offset-l1" id="list-box">
         <div class="row">
-          <div class="col 12">
+          <div class="col s12">
             <div class="row">
               <div class="input-field col s12">
                 <i class="material-icons prefix">search</i>
@@ -142,55 +201,121 @@
             </div>
             <ul class="collections">
               <?php
-                include('db.php');
-                $sql = "SELECT nimi FROM stations ORDER BY nimi ASC";
+                $sql = "SELECT nimi, osoite FROM stations ORDER BY nimi ASC";
                 $res = mysqli_query($db, $sql);
                 $no = 1;
                 while($row = mysqli_fetch_assoc($res)){
-                  echo '
-                  <li class="collection-items">'.$no++. ". ", $row["nimi"].'</li>
-                  ';
+                  $station_nimi = $row["nimi"];
+                  // if the station name is same as the one in the link
+                  if($station_nimi == $station_link_res_name){
+                    echo '
+                    <a href="stations-single.php?station='.$station_nimi.'"><li class="collection-items active  ">
+                    '.$no++, ". ", $station_nimi.'</li></a>
+                    ';
+                  }
+                  // if the station name is same as the first station name in the DB
+                  else if($station_nimi == $first_station_res_name && !isset($_GET['station'])){
+                    echo '
+                    <a href="stations-single.php?station='.$station_nimi.'"><li class="collection-items active  ">
+                    '.$no++, ". ", $station_nimi.'</li></a>
+                    ';
+                  }
+                  else{
+                    echo '
+                    <a href="stations-single.php?station='.$station_nimi.'"><li class="collection-items">
+                    '.$no++, ". ", $station_nimi.'</li></a>
+                    ';
+                  }
                 }
               ?>
-              <!--<li class="collection-items">1. Pasilan asema</li>
-              <li class="collection-items">2. Kauppakorkeakoulu</li>
-              <li class="collection-items">3. Suomenlahdentie</li>
-              <li class="collection-items">4. Armas Launiksen katu</li>
-              <li class="collection-items">5. Leppävaaran urheilupuisto</li>
-              <li class="collection-items">6. Vanha kirkkopuisto</li>
-              <li class="collection-items">7. Eteläinen Hesperiankatu</li>
-              <li class="collection-items">8. Lauttasaaren ostoskeskus</li>
-              <li class="collection-items">9. Mäkkylän asema</li>
-              <li class="collection-items">10. Kapteeninpuistikko</li>
-              <li class="collection-items">11. Hämeenlinnanväylä</li>
-              <li class="collection-items">12. Kaisaniemenpuisto</li>-->
             </ul>
           </div>
         </div>
       </div>
       <!-- Data box -->
-      <div class="col l7 s12 offset-l1" id="data-box"></div>
+      <div class="col l7 s12 " id="data-box">
+        <div class="row">
+          <div class="input-field col s2" id="right-align">
+            <select>
+              <option value="" disabled selected>Filter monthly</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+            </select>
+          </div>
+        </div>
+        <div class="row" id="name-row">
+          <div class="col s5 offset-s1" id ="left-col">
+            <p class="label">Nimi</p>
+            <h5 class="bold-text"><?php echo $station_link_res_name?></h5>
+            <h5><?php echo $station_link_res_namn?></h5>
+
+          </div>
+          <div class="col s5" id ="right-col">
+            <p class="label">Address</p>
+            <h5 class="bold-text"><?php echo $station_link_res_osoite?></h5>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col s5 offset-s1" id ="left-col">
+            <p class="label">Total journeys started</p>
+            <h5 class="bold-text"><?php echo $total_journeys_started_res?></h5>
+          </div>
+          <div class="col s5" id ="right-col">
+            <p class="label">Total journeys ended</p>
+            <h5 class="bold-text"><?php echo $total_journeys_ended_res?></h5>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col s5 offset-s1" id ="left-col">
+            <p class="label">Average distance of started journey</p>
+            <h5 class="bold-text"><?php echo $total_journeys_started_res?></h5>
+          </div>
+          <div class="col s5" id ="right-col">
+            <p class="label">Average distance of ended journey</p>
+            <h5 class="bold-text"><?php echo $total_journeys_ended_res?></h5>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col s8 offset-l2">
+            <div id="map"></div>
+          </div>
+        </div>
+      </div>
     </div>
     
     <!-- js materializecss -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script>
-      // the code will not work probably for the first two clicks. It doesn't remove the first active <li> element. I couldn't solve it.  
-      let list = document.getElementsByClassName('collection-items')
-      for (let i = 0; i < list.length; i++) {
-        list[i].addEventListener("click", function(e){
-          let selectedEl = document.querySelector(".active");
-          if(selectedEl){
-              selectedEl.classList.remove("active");
-          }
-          this.classList.add("active");
-          // Get station names
-          let getStationNames = e.target.textContent;
-          // get only string values, spaces and some symbols
-          const filterStationNames = getStationNames.replace(/[^A-Za-z' ' (),-]/g, '');
-          console.log(filterStationNames);
-          }, false);;
-      }
     </script>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDohh_3rmXgWfj1lv59ll3GT28kKdH7ctk&callback=initMap&v=weekly"
+      defer
+    >
+  </script>
+  <script>
+    function initMap() {
+      const uluru = { lat: <?php echo $station_link_res_y?>, lng: <?php echo $station_link_res_x?> };
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15 ,
+        center: uluru,
+      });
+      const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+      });
+    }
+    window.initMap = initMap;
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, {});
+  });
+  </script>
   </body>
 </html>
+
+<!--AIzaSyDohh_3rmXgWfj1lv59ll3GT28kKdH7ctk-->
