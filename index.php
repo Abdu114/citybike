873c24e86344510fc4c2484dcea2baa6f14f07f7
Abdu114 <abdullahishino123@gmail.com>
2023-01-15 02:25:38 +0200

    moved css to external file, fixed responsiveness, added a loader

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	$mysqli = $db;
 
 	// Get the total number of records from our table "journeys".
-	$total_pages = $mysqli->query('SELECT * FROM journeys WHERE covered_distance > 10 AND duration > 10 LIMIT 60000')->num_rows;
+	$total_pages = $mysqli->query('SELECT * FROM journeys WHERE covered_distance > 10 AND duration > 10 LIMIT 30000')->num_rows;
 
 	// Check if the page number is specified and check if it's a number, if not return the default page number which is 1.
 	$page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
@@ -23,136 +23,17 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 		<!--Import Google Icon Font-->
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<!--Import materialize.css-->
 		<link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
 		<!--font-->
 		<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+		<!-- External CSS file-->
+    <link rel="stylesheet" href="index.css">
     <title>City Bike || Journeys List VIew</title>
-		<meta charset="utf-8">
-		<style>
-			html {
-				font-family: quicksand;
-				padding: 20px;
-				background-color: #fff;
-			}
-			table{
-				font-size: 1.2rem;
-			}
-			.test2 {
-				padding: 15px ;
-				display: inline-flex;
-				justify-content: space-between;
-				box-sizing: border-box;
-				margin-top: 20px;
-			}
-			.test2 li {
-				box-sizing: border-box;
-				padding-right: 10px;
-			}
-			.test2 li a {
-				box-sizing: border-box;
-				padding: 4px 10px;
-				text-decoration: none;
-				font-size: 1.2rem;
-				color: #616872;
-				border-radius: 2px;
-			}
-			.test2 li a:hover {
-				background-color: #eee;
-				cursor: pointer;
-			}
-			.test2 .currentpage a {
-				background-color: #222;
-				color: #fff;
-			}
-			.test2 .currentpage a:hover {
-				background-color: #000;
-			}
-			.material-icons {
-				font-size: 1.8rem;
-				line-height: 1;
-				margin-top: 1px;
-			}
-			/*remove -bg from previous arrow*/
-			li:first-child a:hover {
-				background: none;
-			}
-			/*remove -bg from next arrow*/
-			li:last-child a:hover {
-				background: none;
-			}
-			.total{
-				font-size: 1.3rem;
-				letter-spacing: 2px;
-				font-weight: 600;
-			}
-			.total_num{
-				margin-left: 10px;
-			}
-      .search{
-        padding: 2rem;
-        margin-top: 100px;
-      }
-      .card{
-        font-size: 1.2rem;
-        display: none;
-				width: 91%;
-				margin-left: 47px;
-				box-shadow: none;
-      }
-      .card .card-content{
-        padding: 24px 25px;
-      }
-      #row_journey{
-        margin-bottom: 33px;
-        margin-top: 5px;
-      }
-      #row{
-        margin-bottom: 40px;
-				border-bottom: 1px solid #0000001f;
-      }
-      #bold_text{
-        font-weight: 600;
-      }
-      input[type=number]:not(.browser-default):focus:not([readonly]), input.valid[type=number]:not(.browser-default){
-        border-bottom: 1px solid #000;
-        box-shadow: 0 1px 0 0 #000;
-      }
-      input[type=number]:not(.browser-default):focus:not([readonly])+label{
-        color: #000;
-      }
-      .input-field .prefix.active{
-        color: #000;
-      }
-			nav{
-				background-color: #000;
-				color: #fff;
-				width: 90%;
-				margin: auto;
-			}
-			#nav-center {
-				display:flex;
-				justify-content: center;
-			}
-			nav ul li.active a{
-				background-color: #fff;
-				color: #222;
-			}
-			nav ul li.active a, nav ul li.active a:hover{
-				background-color: #fff;
-				color: #222;
-			}
-			nav ul li:hover{
-				background-color: #ffffff50;
-			}
-			@media only screen and (max-width: 992px) {
-				nav{
-					width: 100%;
-				}
-			}
-		</style>
 	</head>
 	<body>
 		<nav>
@@ -249,6 +130,13 @@
 				</form>
 				<div class="card" id="card">
 					<!-- Here comes the ajax result -->
+					<div class="row">
+						<div class="col s12">
+							<div class="center" id="loader">
+								<img src="loader.svg" alt="Loading">
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
